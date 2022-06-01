b8700a7f804b20835b9b35081e762d1da00e83eb
StayAndRun <neutron.90@mail.ru>
2022-06-02 00:48:52 +0400

    Fixed file watch events

--- src/main/java/com/karatitza/converters/inkscape/console/FileAcceptListener.java
+++ src/main/java/com/karatitza/converters/inkscape/console/FileAcceptListener.java
@@ -8,6 +8,7 @@ import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.concurrent.TimeUnit;
 
+import static com.karatitza.converters.inkscape.console.InkscapeShell.FILE_CREATION_WATCH_TIMEOUT_SECONDS;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
@@ -30,31 +31,36 @@ class FileAcceptListener {
 
     public boolean accept() {
         try {
-            WatchKey watchedKey;
-            boolean acceptFileCreated = false;
-            boolean acceptFileModified = false;
-            while ((watchedKey = watchService.poll(InkscapeShell.FILE_CREATION_WATCH_TIMEOUT_SECONDS, TimeUnit.SECONDS)) != null) {
-                for (WatchEvent<?> watchEvent : watchedKey.pollEvents()) {
-                    WatchEvent.Kind<?> actualEventKind = watchEvent.kind();
-                    InkscapeShell.LOG.info("Accept file Event kind: {}", actualEventKind);
-                    InkscapeShell.LOG.info("Accept file Event context: {}", watchEvent.context());
-                    if (ENTRY_CREATE.equals(actualEventKind)) {
-                        acceptFileCreated = true;
-                        if (watchEvent.count() > 1) InkscapeShell.LOG.warn("Multiple file creation {}", targetFile);
-                    }
-                    if (ENTRY_MODIFY.equals(actualEventKind)) {
-                        acceptFileModified = true;
-                        if (watchEvent.count() > 1) InkscapeShell.LOG.warn("Multiple file modify {}", targetFile);
-                    }
-                    if (acceptFileCreated && acceptFileModified) {
-                        return true;
-                    }
-                }
-                watchedKey.reset();
-            }
-            return false;
+            return tryAccept();
         } catch (InterruptedException e) {
             throw new RuntimeException("File watch canceled: " + targetFile, e);
         }
+    }
+
+    private boolean tryAccept() throws InterruptedException {
+        WatchKey watchedKey;
+        boolean acceptFileCreated = false;
+        boolean acceptFileModified = false;
+        while ((watchedKey = watchService.poll(FILE_CREATION_WATCH_TIMEOUT_SECONDS, TimeUnit.SECONDS)) != null) {
+            for (WatchEvent<?> watchEvent : watchedKey.pollEvents()) {
+                WatchEvent.Kind<?> actualEventKind = watchEvent.kind();
+                String eventContext = String.valueOf(watchEvent.context());
+                InkscapeShell.LOG.info("Accept file Event kind: {}", actualEventKind);
+                InkscapeShell.LOG.info("Accept file Event context: {}", eventContext);
+                if (eventContext.equals(targetFile.getName()) && ENTRY_CREATE.equals(actualEventKind)) {
+                    acceptFileCreated = true;
+                    if (watchEvent.count() > 1) InkscapeShell.LOG.warn("Multiple file creation {}", targetFile);
+                }
+                if (eventContext.equals(targetFile.getName()) && ENTRY_MODIFY.equals(actualEventKind)) {
+                    acceptFileModified = true;
+                    if (watchEvent.count() > 1) InkscapeShell.LOG.warn("Multiple file modify {}", targetFile);
+                }
+                if (acceptFileCreated && acceptFileModified) {
+                    return true;
+                }
+            }
+            watchedKey.reset();
+        }
+        return false;
     }
 }
